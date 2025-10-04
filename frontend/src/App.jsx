7c17063ff07d236c7c37a878fe8ee0be08e05fc6
Sachin Jaiswal <sj586997@gmail.com>
2025-10-04 11:58:52 +0530

    Setup of Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { AppRoutes } from './routes';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <AppRoutes />
         <header className="App-header">
           <h1>ThonHub</h1>
           <p>Hackathon Platform - Coming Soon!</p>
